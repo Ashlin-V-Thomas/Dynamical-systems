3d1fe0177fc3bb833bb2e7fca33cba874026a1e2
Ashlin-V-Thomas <139203657+Ashlin-V-Thomas@users.noreply.github.com>
2023-07-13 09:45:37 +0530

    Update Numerical_integration(Euler's method).py
    Removed graphing and added comments.

--- Numerical_integration(Euler's method).py
+++ Numerical_integration(Euler's method).py
@@ -1,13 +1,11 @@
-import matplotlib.pyplot as plt
 import math
 
 def integrate(initial_condition, final_time, f):
-    #Let x(t) be the solution to differential equation dx/dt = f(x). The function outputs X(final_time).
-    #f=dx/dt
+    #Let x(t) be the solution to differential equation dx/dt = f(x). The function outputs x(final_time).
     #initial_condition is a tuple (x0, t0) that is, x0 = x(t0)
     out = initial_condition[0]
     t=initial_condition[1]
-    #finds a sufficiently small time interval so that  all higher order terms of delta_t goes negligible.
+    #finds a sufficiently small time interval so that our approximation holds .
     delta_t= (final_time - t)/10**2
     while t+delta_t<final_time:
         # Xn+1 = Xn + f(Xn)*delta_t
@@ -17,18 +15,6 @@ def integrate(initial_condition, final_time, f):
     return out
 
 def g(x):
-    return (1-x**2)**0.5
+    return x + math.exp(-x)
 
-def graph_soln(infn,initial_condition):
-    X = []
-    start = initial_condition[1]
-    while start<=initial_condition[1]+10:
-        X.append(start)
-        start += 0.1
-    Y=[]
-    for i in X:
-        Y.append(integrate(initial_condition,i,infn))
-    plt.plot(X,Y)
-    plt.show()
-
-graph_soln(g,(0,0))
+print(integrate((0,0), 1, g))
